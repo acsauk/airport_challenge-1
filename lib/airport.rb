bd6442b5eb73c2407d64e0a2e446a0043481ae7a
acsauk <acsauk@gmail.com>
2016-07-13 22:29:06 +0100

    starts user story #2

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,13 @@ require_relative 'plane'
 
 class Airport
 
-  def land_plane
+  def land_plane(plane)
     Plane.new
+    #plane.in_air? = false
+  end
+
+  def take_off(plane)
+    
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,23 +3,44 @@ require 'airport'
 
 describe Airport do
 
-  #As an air traffic controller
-  #So I can get passengers to a destination
-  #I want to instruct a plane to land at an airport and confirm that it has landed
+  # As an air traffic controller
+  # So I can get passengers to a destination
+  # I want to instruct a plane to land at an airport and confirm that it has landed
 
   #Object          Message
   #
   #Passengers
   #Plane           in_air?
-  #Airport         landed?
+  #Airport         land_plane
 #
-  #Plane   <-- in_air?     --> true/flase
-  #Airport <-- land_plane  --> Plane.new
+  #Plane    <-- in_air?     -->  true/flase
+  #Airport  <-- land_plane  -->  Plane.new
 
-  it { is_expected.to respond_to :land_plane }
+  it { is_expected.to respond_to(:land_plane).with(1).argument }
 
-  it 'lands a plane that is in_air' do
-    plane = subject.land_plane
+  it 'lands a plane' do
+    plane = Plane.new
     expect(plane).not_to be_in_air
+    expect(subject.land_plane(plane)).to eq plane
+  end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+  # Object          Message
+  #
+  # Passengers
+  # Plane           in_air?
+  # Airport         take_off
+
+  # Plane   <--  in_air?   -->  true/false
+  # Airport <--  take_off  -->
+
+  it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  it 'instructs a plane to take_off' do
+    plane = subject.take_off(:plane)
+    expect(plane).to be_in_air
   end
 end
